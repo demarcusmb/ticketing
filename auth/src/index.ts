52ed3e221149671b6339f9a31bf0c65103a324a9
DeMarcus Brown <demarcus.m.brown@gmail.com>
2023-03-11 18:51:46 -0600

    added route + middleware

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,50 +4,49 @@ import { json } from 'body-parser';
 import mongoose from 'mongoose';
 import cookieSession from 'cookie-session';
 
-import { currentUserRouter } from './routes/current-user'
+import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
-import { signupRouter } from './routes/signup';
 import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found';
-
-
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.set('trust proxy', true);
 app.use(json());
 app.use(
-    cookieSession({
-        signed:false,
-        secure: true
-    })
-)
+  cookieSession({
+    signed: false,
+    secure: true
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
-app.use(signupRouter);
 app.use(signoutRouter);
+app.use(signupRouter);
 
-app.all('*', async (req, res, next) => {
-    throw new NotFoundError();
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
 });
 
-app.use(errorHandler)
+app.use(errorHandler);
 
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
 
-    if(!process.env.JWT_KEY) {
-        throw new Error('JWT_KEY must be defined')
-    }
-    try {
-        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
-        console.log('Connect to MongoDB')
-    } catch (err) {
-        console.error(err);
-    }
-    app.listen(3000, () => {
-        console.log('Listening on port 3000!!!!')
-    });
-} 
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    console.log('Connected to MongoDb');
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!!!!!!!!');
+  });
+};
 
 start();
